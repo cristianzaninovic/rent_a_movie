bcf41cb88977135827fbefa18eba3afda05987a4
cristianzaninovic <czaninovic@gmail.com>
2023-08-17 17:23:35 -0400

    models movie and client and relations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :clients
+  resources :movies
   get 'site/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
